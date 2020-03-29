1aae42c266d61c2505401849306e30b7c636a290
梁柱 <ymblender@gmail.com>
2020-03-30 01:54:10 +0800

    fix: clear log

--- lib/index.ts
+++ lib/index.ts
@@ -110,7 +110,6 @@ async function push(path: string, params?: { [key: string]: any }) {
   if (detail.paths.length > 0 && detail.paths[detail.paths.length-1].path === path) {
     return;
   }
-  console.log(detail);
 
   const url = hashStringify(path, params);
 
